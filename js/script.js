00da1806b5f9ef90dca31636fbf7a719f49e6757
Oltinbek2010 <oltinbek04@gmail.com>
2024-07-24 23:16:02 +0500

    commit

---
+++ js/script.js
@@ -0,0 +1,242 @@
+
+// MASSIVGA OID MASALALAR
+
+//  1-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+// let newCar = [];
+
+// for (const iterator of cars) {
+//     newCar.push(iterator, iterator)
+// }
+
+// console.log(newCar);
+
+// 2-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+// let newCar = [];
+
+// for (const iterator of cars) {
+//     newCar.unshift(iterator)
+// }
+
+// console.log(newCar);
+
+// 3-masala
+
+// let nums = [1, -2, 3, -4, 5];
+// let newNums = [];
+
+// for (const iterator of nums) {
+//     if (iterator > 0) {
+//         newNums.push(iterator)
+//     }
+// }
+
+// console.log(newNums);
+
+// 4-masala
+
+// let nums = [1, 2, 3, 4, 5, 6];
+// let sum = 0;
+
+// for (const iterator of nums) {
+//     sum += iterator;
+// } 
+
+// console.log(sum);
+
+// 5-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+
+// cars.push('Rolls Royce');
+
+// console.log(cars);
+
+
+// 6-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+
+// cars.pop();
+
+// console.log(cars);
+
+// 7-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+
+// cars.shift();
+
+// console.log(cars);
+
+// 8-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+
+// cars.unshift('Rolls Royce');
+
+// console.log(cars);
+
+// 9-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+
+// console.log(cars.indexOf('BMW'));
+
+// 10-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+// let newCar = [];
+
+// for (const iterator of cars) {
+//     newCar.push(iterator);
+// }
+
+// console.log(newCar);
+
+// 11-masala
+
+// let nums = [1, 2, 3, 4, 5, 6];
+// let max = nums[0];
+
+// for (const iterator of nums) {
+//     if (max < iterator) {
+//         max = iterator;
+//     }
+// }
+
+// console.log(max);
+
+// 12-masala
+
+// let nums = [1, 2, 3, 4, 5, 6];
+// let min = nums[0];
+
+// for (const iterator of nums) {
+//     if (min > iterator) {
+//         min = iterator;
+//     }
+// }
+
+// console.log(min);
+
+// 13-masala
+
+// let nums = [1, 2, 3, 4, 5, 6];
+// let counter = 0;
+
+// for (const iterator of nums) {
+//     if (iterator % 2 == 0) {
+//         counter ++;
+//     }
+// }
+
+// console.log(counter);
+
+// 14-masala
+
+// let nums = [1, 2, 3, 4, 5, 6];
+// let counter = 0;
+
+// for (const iterator of nums) {
+//     if (iterator % 2 == 1) {
+//         counter ++;
+//     }
+// }
+
+// console.log(counter);
+
+// 15-masala
+
+// let array = [1, 0, 5, 6, 0, 2, 0, 3];
+
+// let newArray = array.filter(element => element !== 0);
+
+// console.log(newArray); 
+
+// 16-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+// let newCar = [];
+
+// for (const iterator of cars) {
+//     newCar.push(iterator, iterator)
+// }
+
+// console.log(newCar);
+
+// 17-masala
+
+// TUSHUNMADIM!!!
+
+// 18-masala
+
+// let nums = [1, 2, 3, 4, 5, 6];
+// let p = 1;
+
+// for (const iterator of nums) {
+//     p *= iterator;
+// }
+
+// console.log(p);
+
+// 19-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+// let newCar = [];
+
+// for (let i = 0; i < cars.length; i++) {
+//     if (i % 2 == 1) {
+//         newCar.push(cars[i])
+//     }
+// }
+
+// console.log(newCar);
+
+// 20-masala
+
+// let cars = ['BMW', 'MERC', 'TOYOTA', 'TESLA'];
+// let newCar = [];
+
+// for (let i = 0; i < cars.length; i++) {
+//     if (i % 2 == 0) {
+//         newCar.push(cars[i])
+//     }
+// }
+
+// console.log(newCar);
+
+// 21-masala
+
+// let array = ['BMW', 1, 'MERC', true, 'TOYOTA', 2n, 'TESLA'];
+// let newArray = [];
+
+// for (const iterator of array) {
+//     if (typeof iterator == 'string') {
+//         newArray.push(iterator);
+//     }
+// }
+
+// console.log(newArray);
+
+// 22-masala
+
+// let array = ['BMW', 1, 'MERC', true, 'TOYOTA', 2n, 'TESLA'];
+// let newArray = [];
+
+// for (const iterator of array) {
+//     if (typeof iterator == 'number') {
+//         newArray.push(iterator);
+//     }
+// }
+
+// console.log(newArray);
+
+// 23-masala
+
+let array = ['BMW', 1, 'MERC', true, 'TOYOTA', 2n, 'TESLA'];
+
+console.log(array.length);
